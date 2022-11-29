8d42db9290625090392eaebc918f03b8fef9e7fb
MiguelGmz7 <oscar.gomez5178@alumnos.udg.mx>
2022-11-29 02:47:50 -0600

    dormi

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func ModString(s string) string {
         // like first with last and so on.
         rns[i], rns[j] = rns[j], rns[i]
     }
-  
+
+	for l = 0; l <= len(rns)-1; l++ {
+		if(rns[l] = 3) {
+			fmt.Println("hola")
+		}
+	}
 
     return string(rns)
 }
